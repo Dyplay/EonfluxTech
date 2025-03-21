67621d541b137a4806ed6345db19f6743ecb62e1
Dyplay <71791638+Dyplay@users.noreply.github.com>
2025-03-21 07:55:20 +0100

    Update AdminBlogContent.tsx

--- src/app/admin/blog/AdminBlogContent.tsx
+++ src/app/admin/blog/AdminBlogContent.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-// Move all the existing content from page.tsx to here
-// Keep all the imports and component code the same
 export default function AdminBlogContent() {
-  // ... all your existing code from page.tsx
-} 
+  return (
+    <div>
+      {/* Add your blog admin content here */}
+    </div>
+  );
+}
